230a8621e1161930599a88a486a976486582c842
Caleb Wolff <caleb.wolff@my.simpson.edu>
2018-03-02 14:22:43 -0600

    Added StatusBar and PlayerClass

---
+++ Unity Project/Assets/Sprites/Characters/PCC/PlayerClass.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClass {
+
+    static int health;
+    static int  stamina;
+
+    static double attack = System.Math.Pow(2.0, (stamina + health)/10);
+}
