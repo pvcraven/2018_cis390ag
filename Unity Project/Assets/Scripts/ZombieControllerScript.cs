7bd4dc1231efddcebf7a78f330476b74c582aa00
avl <avlovan@cableone.net>
2018-03-18 15:20:24 -0500

    Edit script for Zombie01 walking

--- Unity Project/Assets/Scripts/ZombieControllerScript.cs
+++ Unity Project/Assets/Scripts/ZombieControllerScript.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZombieControllerScript : MonoBehaviour {
+public class ZombieControllerScript : MonoBehaviour
+{
+    public float maxSpeed = 10f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private bool facingLeft = true;
+    private Rigidbody2D rb;
+    private Animator anim;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        anim.SetFloat("vSpeed", rb.velocity.y);
+
+        var move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(move));
+        rb.velocity = new Vector2(move * maxSpeed, rb.velocity.y);
+
+        if (move > 0 && !facingLeft)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingLeft)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingLeft = !facingLeft;
+        var theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
