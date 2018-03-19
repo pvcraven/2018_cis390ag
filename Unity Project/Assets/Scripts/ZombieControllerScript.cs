78f218e6bff74713da34b9d055ba47ca4f474cb1
Josh Dietrich <josh.dietrich@my.simpson.edu>
2018-03-19 15:40:47 -0500

    Working on zombie controller script

--- Unity Project/Assets/Scripts/ZombieControllerScript.cs
+++ Unity Project/Assets/Scripts/ZombieControllerScript.cs
@@ -5,32 +5,50 @@ using UnityEngine;
 public class ZombieControllerScript : MonoBehaviour
 {
     public float maxSpeed = 10f;
+    public float timeTravelled = 5f;
+    public Transform raycastSpawn;
 
-    private bool facingLeft = true;
+    private bool facingLeft = false;
+    private bool characterFound = false;
+    private float flipTime;
     private Rigidbody2D rb;
-    private Animator anim;
+    //private Animator anim;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        flipTime = Time.time + timeTravelled;
+        //anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
-        anim.SetFloat("vSpeed", rb.velocity.y);
+        //anim.SetFloat("vSpeed", rb.velocity.y);
 
-        var move = Input.GetAxis("Horizontal");
-        anim.SetFloat("Speed", Mathf.Abs(move));
-        rb.velocity = new Vector2(move * maxSpeed, rb.velocity.y);
+        characterFound = checkForPlayer();
 
-        if (move > 0 && !facingLeft)
+        if(!characterFound)
         {
-            Flip();
+            if(Time.time < flipTime)
+            {
+                if(facingLeft)
+                {
+                    rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
+                }
+                else
+                {
+                    rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+                }
+            }
+            else
+            {
+                Flip();
+                flipTime = Time.time + timeTravelled;
+            }
         }
-        else if (move < 0 && facingLeft)
+        else
         {
-            Flip();
+            Debug.Log("Player found!");
         }
     }
 
@@ -40,5 +58,26 @@ public class ZombieControllerScript : MonoBehaviour
         var theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+    }
+    bool checkForPlayer()
+    {
+        //if(facingLeft)
+        //{
+        //    if(Physics2D.Raycast(rb.transform.position, Vector3.back, 1, 13))
+        //    {
+        //        return true;
+        //    }
+        //}
+        //else
+        //{
+        //    if(Physics2D.Raycast(rb.transform.position,Vector3.forward, 1, 13))
+        //    {
+        //        return true;
+        //    }
+        //}
+
+        Debug.Log(Physics2D.Raycast(raycastSpawn.position, Vector3.forward, 1, 13).collider.gameObject.tag);
+
+        return false;
     }
 }
