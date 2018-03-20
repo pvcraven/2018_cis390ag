b92aac3ae55f669ec9bf794a7f766016d07d857e
Nathan Hawkins <natehawk@live.com>
2018-03-20 10:54:51 -0500

    Modified the player's jumping to be less buggy. No more wall hugging and double jumping.

--- Unity Project/Assets/Scripts/PlayerControllerScript.cs
+++ Unity Project/Assets/Scripts/PlayerControllerScript.cs
@@ -1,267 +1,248 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading;
 using UnityEngine;
 
 public class PlayerControllerScript : MonoBehaviour
 {
-   #region Attributes
-   public float maxSpeed = 5f;
-   public float jumpForce = 5f;
-    public float groundDistance;
-   public float fallMultiplier = 2f;
-   public float lowJumpMultiplier = 2f;
-    public GameObject bullet;
-    public Transform bulletSpawn;
-    public float fireRate = 1f;
+	#region Attributes
 
-    // If the character begins the level facing left, this needs to be changed to false.
-    private bool facingRight = true;
-   private bool isGrounded = true;
-    private bool stabbing = false;
-    private bool hasWeapon = true;
-    private float nextFire;
-    #endregion
+	public float maxSpeed = 5f;
+	public float jumpForce = 5f;
+	public float groundDistance;
+	public float fallMultiplier = 2f;
+	public float lowJumpMultiplier = 2f;
+	public GameObject bullet;
+	public Transform bulletSpawn;
+	public float fireRate = 1f;
 
-    #region Components
-    GameObject[] food;
-    GameObject[] weapons;
-    GameObject[] items;
-    GameObject[] enemies;
-    Animator anim;
-    #endregion
+	// If the character begins the level facing left, this needs to be changed to false.
+	private bool facingRight = true;
+	private bool isGrounded = false;
+	private bool stabbing = false;
+	private bool hasWeapon = true;
+	private float nextFire;
+	private List<Collision2D> groundPlayerIsTouching = new List<Collision2D>();
+	private RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+	private List<RaycastHit2D> hitBufferList = new List<RaycastHit2D> (16);
+	private ContactFilter2D contactFilter;
 
-    void Start()
-   {
-      Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
-      Collider2D collider2D = GetComponent<Collider2D>();
-      
-        anim = GetComponent<Animator>();
-      rigidbody2D = GetComponent<Rigidbody2D>();
-        collider2D = GetComponent<Collider2D>();
+	#endregion
 
-        food = GameObject.FindGameObjectsWithTag("Food");
-        weapons = GameObject.FindGameObjectsWithTag("Weapon");
-        items = GameObject.FindGameObjectsWithTag("Item");
-        enemies = GameObject.FindGameObjectsWithTag("Enemy");
-    }
+	#region Components
 
-   // Use when applying non-physics-related functions. Runs once per frame.
-   void Update()
-   {
-      CheckForInput();
-      
-      // Falling
-      //CheckIfGrounded();
-      ApplyFallMultipliers();
-        CheckIfTouchingItems();
-        CheckIfTouchingEnemy();
-      CheckIfGrounded();
-   }
+	GameObject[] food;
+	GameObject[] weapons;
+	GameObject[] items;
+	GameObject[] enemies;
+	Animator anim;
+	private Rigidbody2D rigidbody2D;
+	private Collider2D collider2D;
+
+	#endregion
+
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+		rigidbody2D = GetComponent<Rigidbody2D>();
+		collider2D = GetComponent<Collider2D>();
+		contactFilter.useTriggers = false;
+		contactFilter.SetLayerMask (Physics2D.GetLayerCollisionMask (gameObject.layer));
+		contactFilter.useLayerMask = true;
+
+		food = GameObject.FindGameObjectsWithTag("Food");
+		weapons = GameObject.FindGameObjectsWithTag("Weapon");
+		items = GameObject.FindGameObjectsWithTag("Item");
+		enemies = GameObject.FindGameObjectsWithTag("Enemy");
+	}
+
+	// Use when applying non-physics-related functions. Runs once per frame.
+	void Update()
+	{
+		CheckForInput();
+
+		// Falling
+		//CheckIfGrounded();
+		ApplyFallMultipliers();
+		CheckIfTouchingItems();
+		CheckIfTouchingEnemy();
+		CheckIfGrounded();
+	}
 
 // Use when applying physics-related functions. Runs in sync with the physics engine - may update 0, 1, or many times per frame depending on the physics FPS settings.
-    void FixedUpdate()
-   {
-      CheckForInput();
-        stabbing = Input.GetKeyDown("f");
+	void FixedUpdate()
+	{
+		CheckForInput();
+		stabbing = Input.GetKeyDown("f");
 
-        if(stabbing)
-        {
-            Stab();
-        }
-   }
+		if (stabbing)
+		{
+			Stab();
+		}
+	}
 
-   #region Logic Functions
-   /// <summary>
-   /// Checks for user input.
-   /// </summary>
-   private void CheckForInput()
-   {
-      if (Input.GetButtonDown("Jump") && isGrounded)
-      {
-         Jump();
-      }
+	#region Logic Functions
 
-        if(Input.GetButton("Shoot") && hasWeapon && Time.time > nextFire)
-        {
-            nextFire = Time.time + fireRate;
-            FireWeapon();
-        }
-      
-      MoveHorizontally();
-   }
+	private void CheckForInput()
+	{
+		if (Input.GetButtonDown("Jump") && this.isGrounded)
+		{
+			Jump();
+		}
 
-    private void CheckIfTouchingItems()
-    {
-       Collider2D collider2D = GetComponent<Collider2D>();
-       
-        foreach (GameObject item in food)
-        {
-            if (item.GetComponent<Collider2D>().IsTouching(collider2D))
-            {
-                Debug.Log("Colliding with item");
-                if (Input.GetButton("Interact"))
-                {
-                    Destroy(item);
-                }
-            }
-        }
-        foreach (GameObject item in weapons)
-        {
-            if (item.GetComponent<Collider2D>().IsTouching(collider2D))
-            {
-                if (Input.GetButton("Interact"))
-                {
-                    Destroy(item);
-                }
-            }
-        }
-        foreach (GameObject item in items)
-        {
-            if (item.GetComponent<Collider2D>().IsTouching(collider2D))
-            {
-                if (Input.GetButton("Interact"))
-                {
-                    Destroy(item);
-                }
-            }
-        }
-    }
+		if (Input.GetButton("Shoot") && hasWeapon && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			FireWeapon();
+		}
 
-    private void CheckIfTouchingEnemy()
-    {
-       Collider2D collider2D = GetComponent<Collider2D>();
-       
-        foreach (GameObject enemy in enemies)
-        {
-            if (enemy.GetComponent<Collider2D>().IsTouching(collider2D))
-            {
-                Debug.Log("Colliding with enemy");
-                //Code to add functionality when collision is detected, like attacking
-            }
-        }
-    }
+		MoveHorizontally();
+	}
 
-   private void OnCollisionEnter2D(Collision2D other)
-   {
-      if (other.gameObject.tag.Equals("Ground"))
-      {
-         isGrounded = true;
-      }
-   }
+	private void CheckIfTouchingItems()
+	{
+		foreach (GameObject item in food)
+		{
+			if (item.GetComponent<Collider2D>().IsTouching(collider2D))
+			{
+				Debug.Log("Colliding with item");
+				if (Input.GetButton("Interact"))
+				{
+					Destroy(item);
+				}
+			}
+		}
 
-   private void OnCollisionExit2D(Collision2D other)
-   {
-      if (other.gameObject.tag.Equals("Ground"))
-      {
-         StartCoroutine(WaitToJumpInSeconds(.2f));
-         //isGrounded = false;
-         //print("Exiting Ground");
-      }
-   }
+		foreach (GameObject item in weapons)
+		{
+			if (item.GetComponent<Collider2D>().IsTouching(collider2D))
+			{
+				if (Input.GetButton("Interact"))
+				{
+					Destroy(item);
+				}
+			}
+		}
 
-   //TODO: Wait for a split second before "exiting ground" so the player can jump while running on slants
-   IEnumerator WaitToJumpInSeconds(float seconds)
-   {
-      yield return new WaitForSeconds(seconds);
-      isGrounded = false;
-   }
-   
-   /// <summary>
-    /// Checks if the user is on the ground or not and modifies the isGrounded field accordingly.
-    /// </summary>
-    private void CheckIfGrounded()
-   {
-      Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
-      if (rigidbody2D.velocity.y.Equals(0))
-      {
-         isGrounded = true;
-      }
-   }
-   #endregion
-   
-   #region Movement Functions
-   /// <summary>
-   /// Flips the player's sprite's direction.
-   /// </summary>
-   private void FlipDirection()
-   {
-      facingRight = !facingRight;
-      Vector2 scale = transform.localScale;
-      scale.x *= -1;
-      transform.localScale = scale;
-   }
+		foreach (GameObject item in items)
+		{
+			if (item.GetComponent<Collider2D>().IsTouching(collider2D))
+			{
+				if (Input.GetButton("Interact"))
+				{
+					Destroy(item);
+				}
+			}
+		}
+	}
 
-   /// <summary>
-   /// Causes the player to jump.
-   /// </summary>
-   private void Jump()
-   {
-      Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
-      
-      rigidbody2D.velocity = Vector2.up * jumpForce;
-      this.isGrounded = false;
-   }
+	private void CheckIfTouchingEnemy()
+	{
+		foreach (GameObject enemy in enemies)
+		{
+			if (enemy.GetComponent<Collider2D>().IsTouching(collider2D))
+			{
+				Debug.Log("Colliding with enemy");
+				//Code to add functionality when collision is detected, like attacking
+			}
+		}
+	}
 
-   /// <summary>
-   /// Causes the player to fall. The speed of the player's fall depends on how long they hold the Jump key. This allows
-   /// the user to either "short" jump or "long" jump. 
-   /// </summary>
-   private void ApplyFallMultipliers()
-   {
-      Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
-      
-      if (rigidbody2D.velocity.y < 0)
-      {
-         rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-      }
-      else if (rigidbody2D.velocity.y > 0 && !Input.GetButton("Jump"))
-      {
-         rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
-      }
-   }
+	private void CheckIfGrounded()
+	{
+		this.isGrounded = false;
+		int count = rigidbody2D.Cast (new Vector2(0, -1), contactFilter, hitBuffer, .2f);
+		hitBufferList.Clear();
 
-   /// <summary>
-   /// Takes input from the user along the y axis and moves the player accordingly.
-   /// </summary>
-   private void MoveHorizontally()
-   {
-      Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
-      
-      float move = Input.GetAxis("Horizontal");
-        if(Input.GetButton("Sprint"))
-        {
-            rigidbody2D.velocity = new Vector2(move * maxSpeed * 1.5f, rigidbody2D.velocity.y);
-        }
-        else
-        {
-            rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
-        }
+		foreach (RaycastHit2D element in hitBuffer)
+		{
+			hitBufferList.Add(element);
+		}
 
-      // Flip the character if they're moving in the opposite direction
-      if (move > 0 && !facingRight)
-      {
-         FlipDirection();
-      }
-      else if (move < 0 && facingRight)
-      {
-         FlipDirection();
-      }
-   }
+		for (int i = 0; i < hitBufferList.Count; i++) 
+		{
+			Vector2 currentNormal = hitBufferList[i].normal;
+			if (currentNormal.y > .01f) 
+			{
+				this.isGrounded = true;
+			}
+		}
+	}
 
-    private void Stab()
-    {
-        anim.SetBool("stabbing", stabbing);
-        anim.Play("Tory_Stabbing");
-        stabbing = false;
-        anim.SetBool("stabbing", stabbing);
-    }
+	#endregion
 
-    private void FireWeapon()
-    {
-        Instantiate(bullet, bulletSpawn.position, Quaternion.identity);
-    }
+	#region Movement Functions
 
-    #endregion
+	private void FlipDirection()
+	{
+		facingRight = !facingRight;
+		Vector2 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+	
+	private void Jump()
+	{
+		this.isGrounded = false;
+		rigidbody2D.velocity = Vector2.up * jumpForce;
+	}
+
+	/// <summary>
+	/// Causes the player to fall. The speed of the player's fall depends on how long they hold the Jump key. This allows
+	/// the user to either "short" jump or "long" jump. 
+	/// </summary>
+	private void ApplyFallMultipliers()
+	{
+		if (rigidbody2D.velocity.y < 0)
+		{
+			rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+		}
+		else if (rigidbody2D.velocity.y > 0 && !Input.GetButton("Jump"))
+		{
+			rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+		}
+	}
+
+	/// <summary>
+	/// Takes input from the user along the y axis and moves the player accordingly.
+	/// </summary>
+	private void MoveHorizontally()
+	{
+		float move = Input.GetAxis("Horizontal");
+		if (Input.GetButton("Sprint"))
+		{
+			rigidbody2D.velocity = new Vector2(move * maxSpeed * 1.5f, rigidbody2D.velocity.y);
+		}
+		else
+		{
+			rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+		}
+
+		// Flip the character if they're moving in the opposite direction
+		if (move > 0 && !facingRight)
+		{
+			FlipDirection();
+		}
+		else if (move < 0 && facingRight)
+		{
+			FlipDirection();
+		}
+	}
+
+	private void Stab()
+	{
+		anim.SetBool("stabbing", stabbing);
+		anim.Play("Tory_Stabbing");
+		stabbing = false;
+		anim.SetBool("stabbing", stabbing);
+	}
+
+	private void FireWeapon()
+	{
+		Instantiate(bullet, bulletSpawn.position, Quaternion.identity);
+	}
+
+	#endregion
 }
