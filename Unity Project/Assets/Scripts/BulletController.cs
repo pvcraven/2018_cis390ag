e0829e28e14c384927000cbf356e98f6bece0d85
SC\keegan.lamparek <keeganlamparek@gmail.com>
2018-03-23 15:27:36 -0500

    Redoing Bullet damaging, still in progress.

--- Unity Project/Assets/Scripts/BulletController.cs
+++ Unity Project/Assets/Scripts/BulletController.cs
@@ -5,32 +5,25 @@ using UnityEngine;
 public class BulletController : MonoBehaviour {
 
     public float bulletSpeed = 8f;
-
-    private Rigidbody2D rb2D;
-    private Collider2D coll2D;
     public GameObject zombie;
     public GameObject bandit;
 
 
 	// Use this for initialization
 	void Start () {
-        rb2D = GetComponent<Rigidbody2D>();
-        coll2D = GetComponent<Collider2D>();
-
-        rb2D.velocity = new Vector2(bulletSpeed, rb2D.velocity.x);
+       
+        GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed, GetComponent<Rigidbody2D>().velocity.x);
     }
 
-    void FixedUpdate()
+    void OnTriggerEnter(Collider other)
     {
-        DetectCollision();
-    }
+        Debug.Log("BulletTriggerEnter");
 
-    void DetectCollision()
-    {
-        if (coll2D.IsTouching(zombie.GetComponent<Collider2D>()))
+        if(other.gameObject.tag == "Enemy")
         {
-            zombie.GetComponent<ZombieControllerScript>().health -= 50f;
-            Destroy(rb2D.gameObject);
+            other.gameObject.GetComponent<ZombieControllerScript>().TakeDamage();
+
         }
     }
+
 }
