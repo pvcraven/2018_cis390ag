584927c3d03a04eb2d6b7f67b4ac58be04b73382
SC\keegan.lamparek <keeganlamparek@gmail.com>
2018-03-28 15:27:52 -0500

    Starting ranged weapon interface and gun class

---
+++ Unity Project/Assets/Scripts/Interfaces/IRangedWeaponInterface.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IRangedWeaponInterface{
+
+
+    int AmmuntionSpeed { get; set; }
+    int AmmunitionDamage { get; set; }
+    int FireRate { get; set; }
+
+    void OnTriggerEnter2D(Collider2D other);
+
+    void OnTriggerExit2D(Collider2D other);
+}
